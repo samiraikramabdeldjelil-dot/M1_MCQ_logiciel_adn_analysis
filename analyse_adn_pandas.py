6350a11d5667ee4b6590769397e6f79cc2c23ec8
samiraikramabdeldjelil-dot <samiraikramabdeldjelil@gmail.com>
2025-12-10 20:42:48 +0100

    Add author and course details to analyse_adn_pandas.py
    Added author and course information as comments.

--- analyse_adn_pandas.py
+++ analyse_adn_pandas.py
@@ -1,1 +1,8 @@
+#abdeldjelil samira ikram,master 1 Microbiology and quality control,10/12/2025
+#dib merwa
+#tharaoui hanene
+#belamri merwa
+#messaoudi fatima
+#dali youcef ines
 
+import pandas as pd
